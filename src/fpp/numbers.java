5ffd665f54636c0f6346367de451bc7b85ce589a
Sanjeev Shrestha <sanjeevshrestha2004@gmail.com>
2015-10-30 16:23:48 -0500

    Numbers

---
+++ src/fpp/numbers.java
@@ -0,0 +1,22 @@
+package fpp;
+
+public class numbers {
+
+	/**
+	 * An integer is defined to be an even subset of another integer n if every even factor of m is also a factor of n.
+For example 18 is an even subset of 12 because the even factors of 18 are 2 and 6 and these are both factors of 12.
+But 18 is not an even subset of 32 because 6 is not a factor of 32.
+	 */
+	
+	public int isEvenSubset(int m, int n)
+	{
+		int isEvenFactor=1;
+		for(int x=2;x<=m/2;x++)
+		{
+			
+			
+		}
+		
+		return isEvenFactor;
+	}
+}
---
+++ src/fpp/numbersTest.java
@@ -0,0 +1,14 @@
+package fpp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class numbersTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
